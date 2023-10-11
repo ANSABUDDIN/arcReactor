dda35fabfe6499b67982d3fbda22d8beb62f0649
ANSABUDDIN <ansabuddin0346@gmail.com>
2023-10-10 18:00:46 -0700

    new changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   // This function will run when the DOM has finished loading
-  setTimeout(() => {
-    hideLoader();
-  }, 200);
+
+  hideLoader();
 });
 
 function hideLoader() {
   const loader = document.querySelector(".loader");
-  const content = document.quserySelector(".content");
 
   loader.style.display = "none";
-  content.classList.remove("invisible");
 }
